c5ef852cb75f806c93a1ef065de305968ca38944
R3tromanCZ <steinm07@gmail.com>
2018-12-12 21:18:31 +0100

    Version 0.2

--- src/main/java/cz/mstein/minecraft/uwm/init/UWMReference.java
+++ src/main/java/cz/mstein/minecraft/uwm/init/UWMReference.java
@@ -3,6 +3,6 @@ package cz.mstein.minecraft.uwm.init;
 public class UWMReference {
 	public static final String MODID = "uwm";
 	public static final String MODNAME = "Upgradable Wands Mod";
-	public static final String VERSION = "0.1";
+	public static final String VERSION = "0.2";
 	public static final String ACCEPTED_MINECRAFT_VERSIONS = "[1.12]";
 }
