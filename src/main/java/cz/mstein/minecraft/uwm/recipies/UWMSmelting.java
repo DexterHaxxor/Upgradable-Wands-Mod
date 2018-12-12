d4306ba3f1a6e7cac004f621278c3634a24e27bc
R3tromanCZ <steinm07@gmail.com>
2018-12-12 21:16:56 +0100

    Added Perpium Ore smelting recipie

--- src/main/java/cz/mstein/minecraft/uwm/recipies/UWMSmelting.java
+++ src/main/java/cz/mstein/minecraft/uwm/recipies/UWMSmelting.java
@@ -1,5 +1,6 @@
 package cz.mstein.minecraft.uwm.recipies;
 
+import cz.mstein.minecraft.uwm.blocks.UWMBlocks;
 import cz.mstein.minecraft.uwm.items.UWMItems;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.registry.GameRegistry;
@@ -9,5 +10,6 @@ public class UWMSmelting {
 	public static void init() {
 		GameRegistry.addSmelting(UWMItems.javium_dust, new ItemStack(UWMItems.javium_ingot, 1), 1.5f);
 		GameRegistry.addSmelting(UWMItems.perpium_dust, new ItemStack(UWMItems.perpium_ingot, 1), 1.5f);
+		GameRegistry.addSmelting(UWMBlocks.perpium_ore, new ItemStack(UWMItems.perpium_ingot, 1), 1.5f);
 	}	
 }
