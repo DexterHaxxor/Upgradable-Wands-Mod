40b9cb34ae71d21fa51d1bdcef6768f80b792e5b
R3tromanCZ <steinm07@gmail.com>
2018-12-21 13:47:52 +0100

    making the registry

--- src/main/java/cz/mstein/minecraft/uwm/items/wand/UWMWand.java
+++ src/main/java/cz/mstein/minecraft/uwm/items/wand/UWMWand.java
@@ -3,22 +3,8 @@ package cz.mstein.minecraft.uwm.items.wand;
 import cz.mstein.minecraft.uwm.items.UWMItem;
 
 public class UWMWand extends UWMItem {
-	protected UWMWand(int type) {
+	public UWMWand() {
 		super("wand");
-		String itemname;
-		switch(type) {
-			case 0: itemname = "basic_wand";
-				break;
-			case 1: itemname = "advanced_wand";
-				break;
-			case 2: itemname = "extreme_wand";
-				break;
-			case 3: itemname = "ultimate_wand";
-				break;
-			default: itemname = "basic_wand";
-		}
-		this.setUnlocalizedName(itemname);
-		this.setRegistryName(itemname);
 	}
 }
 
---
+++ src/main/java/cz/mstein/minecraft/uwm/items/wand/WandGadget.java
@@ -0,0 +1,7 @@
+package cz.mstein.minecraft.uwm.items.wand;
+
+public class WandGadget extends net.minecraftforge.registries.IForgeRegistryEntry.Impl<WandGadget> {
+	public WandGadget(String id) {
+		
+	}
+}
