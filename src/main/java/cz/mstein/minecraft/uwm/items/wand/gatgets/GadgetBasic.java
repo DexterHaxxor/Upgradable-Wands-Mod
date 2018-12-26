8d3dd7a6b0f748aed6c6530cf274919792043ba9
R3tromanCZ <steinm07@gmail.com>
2018-12-26 10:34:37 +0100

    Update GadgetBasic.java

--- src/main/java/cz/mstein/minecraft/uwm/items/wand/gatgets/GadgetBasic.java
+++ src/main/java/cz/mstein/minecraft/uwm/items/wand/gatgets/GadgetBasic.java
@@ -1,8 +1,8 @@
 package cz.mstein.minecraft.uwm.items.wand.gatgets;
 
 import cz.mstein.minecraft.uwm.items.wand.WandGadget;
-import net.minecraft.block.BlockWorkbench;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumHand;
 import net.minecraft.world.World;
 
@@ -13,5 +13,6 @@ public class GadgetBasic extends WandGadget {
 	@Override
 	public void exec(World world, EntityPlayer player, EnumHand hand, String mode) {
 		 player.dropItem(player.getHeldItem(hand), false);
+		 player.setHeldItem(hand, ItemStack.EMPTY);
 	}
 }
